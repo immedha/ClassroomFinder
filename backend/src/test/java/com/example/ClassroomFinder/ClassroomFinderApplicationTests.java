a5f1612c68fe8e27e4fe2e04e996e6e7c59d1dfd
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-10-29 18:38:24 -0700

    Add backend

---
+++ backend/src/test/java/com/example/ClassroomFinder/ClassroomFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClassroomFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
