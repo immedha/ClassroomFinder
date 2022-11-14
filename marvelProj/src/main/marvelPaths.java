e7fcba7c2b543b69979041210a799cfddd40f6c3
Ray Li <raynli@outlook.com>
2022-11-13 16:34:41 -0800

    necessary methods in marvelPaths

--- marvelProj/src/main/marvelPaths.java
+++ marvelProj/src/main/marvelPaths.java
@@ -1,1 +1,5 @@
 //starting marvel project
+//main method: asks users what file to create graph from, scans file in using scanner, calls build graph method
+//      asks users for two nodes, calls find path
+//Graph<String, String> build graph method(string filename): sets global graph variable
+//List<String> findPaths(Graph g, String start, String dest): returns path
